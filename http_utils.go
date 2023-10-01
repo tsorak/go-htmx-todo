442074c54b47925de32d3be6e7eb920ba4947cc0
tsorak <karost0249@gmail.com>
2023-10-01 09:58:04 +0200

    Add getFileContentsIntoString function

--- http_utils.go
+++ http_utils.go
@@ -12,15 +12,26 @@ func accepts(pattern string, r *http.Request) bool {
 	return strings.Contains(accepts, pattern) || strings.Contains(accepts, "*/*")
 }
 
-func getFileContents(path string) (string, error) {
+func getFileContents(path string) ([]byte, error) {
 	absPath, _ := filepath.Abs(path)
 	fsfile, err := os.Open(absPath)
 	if err != nil {
-		return "", err
+		return make([]byte, 0), err
 	}
 
 	bytes := make([]byte, 1024)
 	fsfile.Read(bytes)
+
+	return bytes, nil
+}
+
+func getFileContentsIntoString(path string) (string, error) {
+	absPath, _ := filepath.Abs(path)
+	bytes, err := getFileContents(absPath)
+
+	if err != nil {
+		return "", err
+	}
 
 	return string(bytes), nil
 }
